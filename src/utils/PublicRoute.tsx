b337c63052e9090357bf9f1473c153ef9d3d280a
Rosintan Sitinjak <2381014@unai.edu>
2025-04-14 10:34:55 +0700

    Push handson-11

---
+++ src/utils/PublicRoute.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react"
+import { useAuth } from "./AuthProvider"
+import { Navigate } from "react-router-dom";
+
+
+const PublicRoute = ({ children }: { children: ReactNode }) => {
+    const { isAuthenticated } = useAuth();
+    return isAuthenticated ? <Navigate to="/" /> : children;
+};
+
+export default PublicRoute
